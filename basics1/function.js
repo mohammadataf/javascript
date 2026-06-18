ed552bfd1324cbd73e942fae6585cf1eecebde5b
mohammadataf <atafmohd32@gmail.com>
2026-06-18 21:06:24 +0000

    function

---
+++ basics1/function.js
@@ -0,0 +1,15 @@
+function addtwonumbers(number1,number2){
+    return number1 +number2;
+}
+const result=addtwonumbers(2,3);
+console.log(result);
+function username(name){
+    console.log(`${name} just logged in`);
+    
+}
+username("ataf")
+function calculateprice(...num){
+    return num
+}
+console.log(calculateprice(100,300,3,445,666));
+
